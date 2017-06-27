ccb299b4c1eff6808e7e8ad95d62a11343d2ad00
Michael Wright <michaelw90@users.noreply.github.com>
2017-06-27 14:41:22 +0100

    Force to create?

--- recipes/binary.rb
+++ recipes/binary.rb
@@ -4,7 +4,7 @@ download_dest = File.join(cache_dir, node['wkhtmltopdf-update']['package'])
 remote_file download_dest do
   source node['wkhtmltopdf-update']['mirror_url']
   mode '0644'
-  action :create_if_missing
+  action :create
 end
 
 package 'wkhtmltopdf' do
