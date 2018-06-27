affbd33d6a90c5fd6e70c6a7ee103e6555fa95a2
Fred Thompson <fred.thompson@buildempire.co.uk>
2018-06-27 12:27:10 +0100

    CLA0003-1047 - Update
    Brought in the latest master and then made amendments for the latest download location and structure, and resolved the binary recipe.

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,26 +10,39 @@ default['wkhtmltopdf-update']['version'] = '0.12.4'
 case node['platform_family']
 when 'mac_os_x', 'mac_os_x_server'
   default['wkhtmltopdf-update']['dependency_packages'] = []
-  default['wkhtmltopdf-update']['platform'] = 'osx-cocoa-x86-64.pkg'
+  default['wkhtmltopdf-update']['platform'] = 'macosx-10.9.1-x86_64'
+  default['wkhtmltopdf-update']['package'] = 'osx-cocoa-x86-64.pkg'
 when 'windows'
   default['wkhtmltopdf-update']['dependency_packages'] = []
   default['wkhtmltopdf-update']['platform'] = if node['kernel']['machine'] == 'x86_64'
-                                                'msvc2015-win64.exe'
+                                                'win64'
                                               else
-                                                'msvc2015-win32.exe'
+                                                'win32'
                                               end
+  default['wkhtmltopdf-update']['package'] = if node['kernel']['machine'] == 'x86_64'
+    'msvc2015-win64.exe'
+  else
+    'msvc2015-win32.exe'
+  end
 else
   default['wkhtmltopdf-update']['dependency_packages'] = value_for_platform_family(
     %w[debian] => %w[zlib1g-dev libfontconfig1 libfreetype6-dev libxext6 libx11-dev libxrender1 fontconfig libjpeg8 xfonts-base xfonts-75dpi],
     %w[fedora rhel] => %w[fontconfig libXext libXrender openssl-devel urw-fonts]
   )
-  default['wkhtmltopdf-update']['platform'] = if node['kernel']['machine'] == 'x86_64'
-                                                'linux-generic-amd64.tar.xz'
-                                              else
-                                                'linux-generic-i386.tar.xz'
-                                              end
+
+  if node['kernel']['machine'] == 'x86_64'
+    default['wkhtmltopdf-update']['platform'] = 'linux-amd64'
+    default['wkhtmltopdf-update']['package'] = value_for_platform_family(
+      %w(debian) => "wkhtmltox-#{node['wkhtmltopdf-update']['version']}.trusty-amd64.deb",
+      %w(fedora rhel) => "wkhtmltox-#{node['wkhtmltopdf-update']['version']}.centos6-amd64.rpm"
+    )
+  else
+    default['wkhtmltopdf-update']['platform'] = 'linux-i386'
+    default['wkhtmltopdf-update']['package'] = value_for_platform_family(
+      %w(debian) => "wkhtmltox-#{node['wkhtmltopdf-update']['version']}.trusty-i386.deb",
+      %w(fedora rhel) => "wkhtmltox-#{node['wkhtmltopdf-update']['version']}.centos6-i386.rpm")
+  end
+
 end
 
-default['wkhtmltopdf-update']['archive'] = "wkhtmltox-#{node['wkhtmltopdf-update']['version']}_#{node['wkhtmltopdf-update']['platform']}"
-default['wkhtmltopdf-update']['mirror_url'] = "https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/#{node['wkhtmltopdf-update']['version']}/#{node['wkhtmltopdf-update']['archive']}"
-
+default['wkhtmltopdf-update']['mirror_url'] = "https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/#{node['wkhtmltopdf-update']['version']}/#{node['wkhtmltopdf-update']['package']}"
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jarvis@vortexrevolutions.com'
 license 'Apache-2.0'
 description 'Installs wkhtmltoimage and wkhtmltopdf'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.5.1'
+version '0.5.2'
 chef_version '>= 12.0'
 source_url 'https://github.com/ballisticpain/chef-wkhtmltopdf'
 issues_url 'https://github.com/ballisticpain/chef-wkhtmltopdf/issues'
